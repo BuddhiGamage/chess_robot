c5c23cacb00277673fc7d0a5a7e0f9423b22d079
BuddhiGamage <buddhigamage217@gmail.com>
2025-01-22 17:24:58 +1100

    updated height

--- move_to_x_y.py
+++ move_to_x_y.py
@@ -17,7 +17,7 @@ def check_for_end_or_abort(e):
             e.set()
     return check
 
-def move_to_cartesian_position(base, x, y,z=0.15):
+def move_to_cartesian_position(base, x, y,z=0.12):
     """
     Move the arm to a given Cartesian position with fixed values for other pose parameters.
     
